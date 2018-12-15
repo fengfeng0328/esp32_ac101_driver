ce994236ca37e04b9624c05572e37ce75c6204bf
chen-zifeng <m18819838082@163.com>
2018-12-15 14:15:11 +0800

    fix api

--- components/ac101_driver/AC101_DRV.c
+++ components/ac101_driver/AC101_DRV.c
@@ -15,7 +15,7 @@
 #include "driver/i2s.h"
 #include "driver/i2c.h"
 #include "driver/gpio.h"
-#include "AC101.h"
+#include "AC101_DRV.h"
 #define I2C_MASTER_SCL_IO    32    /*!< gpio number for I2C master clock */
 #define I2C_MASTER_SDA_IO    33    /*!< gpio number for I2C master data  */
 #define I2C_MASTER_NUM I2C_NUM_1   /*!< I2C port number for master dev */
